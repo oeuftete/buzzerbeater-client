4cdfdf15eb1ae3a2c869f7de9608ba11c1f156e5
Ken Crowell <oeuftete@gmail.com>
2009-04-04 12:47:50 +0000

    BB::Player interface via XML added.  Needs tests\!

--- BuzzerBeater/Player.pm
+++ BuzzerBeater/Player.pm
@@ -1,49 +1,104 @@
 #
-#  $Id: Player.pm,v 1.1 2008-10-04 16:56:38 ken Exp $
+#  $Id: Player.pm,v 1.2 2009-04-04 12:47:50 ken Exp $
 #
 
 use strict;
 use warnings;
 
+#  Players are instantiated through the XML interface, but also via rosters.
+#  Players retrieved via XML directly (i.e. player.aspx) have a retrieved and
+#  owner thingy too.
+
 package BuzzerBeater::Player;
 
+use Carp;
+
 use Encode;
+use XML::Twig;
 
 sub new {
-  my $class = shift;
-  my $self = {};
-  bless $self, $class;
-  # Set to something since it's optional in the XML
-  $self->{injury} = 0;
-  $self->{skills} = {};
-  return $self;
+    my $class = shift;
+    my $self  = {};
+    bless $self, $class;
+
+    # Set to something since it's optional in the XML
+    $self->{injury} = 0;
+    $self->{skills} = {};
+
+    $self->_initialize(@_);
+    return $self;
+}
+
+sub _initialize {
+    my $self = shift;
+
+    if (@_) {
+        my $args = shift;
+        $self->setFromXml( $args->{xml} ) if ( exists $args->{xml} );
+    }
+}
+
+sub setFromXml {
+    my $self = shift;
+    my $xml  = shift;
+
+    my $twig = new XML::Twig;
+    $twig->parse($xml);    # safe_parse or croak?
+
+    my $root = $twig->root;
+    my $el   = $root->first_child();
+
+    if ( $el->gi eq 'player' ) {
+
+        #  TODO: Copied from Roster!
+        $self->id( $el->att('id') );
+
+        foreach my $playerEl ( $el->children ) {
+            if ( $playerEl->gi eq 'skills' ) {
+                foreach my $skill ( $playerEl->children ) {
+                    $self->skills( $skill->gi, $skill->text );
+                }
+            }
+            elsif ( $playerEl->gi eq 'nationality' ) {
+                $self->nationality( $playerEl->text, $playerEl->att('id') );
+            }
+            else {
+                $self->basic( $playerEl->gi, $playerEl->text );
+            }
+        }
+    }
+
+    else {
+        carp "Unexpected child element processing player xml: " . $el->gi;
+    }
+    return $self;
 }
 
 #  Get/set player id
 sub id {
-  my $self = shift;
-  if (@_) {
-    $self->{id} = shift;
-    return $self;
-  }
-  return $self->{id};
+    my $self = shift;
+    if (@_) {
+        $self->{id} = shift;
+        return $self;
+    }
+    return $self->{id};
 }
 
 # Get/set the basic (i.e. att-less) top-level fields.
-# 
+#
 # To get, provide the key.
 # To set, provide the key and value.
 sub basic {
-  my $self = shift;
+    my $self = shift;
 
-  if (@_ == 2) {
-    my ($basicGi, $basicText) = @_;
-    $self->{$basicGi} = encode_utf8($basicText);
-  }
-  elsif (@_ == 1) {
-    return $self->{$_[0]};
-  }
-  return $self;
+    if ( @_ == 2 ) {
+        my ( $basicGi, $basicText ) = @_;
+        $self->{$basicGi} = encode_utf8($basicText);
+    }
+    elsif ( @_ == 1 ) {
+        return $self->{ $_[0] };
+    }
+    return $self;
 }
 
 # Get/set the nationality hash.
@@ -55,14 +110,14 @@ sub basic {
 # To get, call with no arguments
 # To set, call with the name and id
 sub nationality {
-  my $self = shift;
-  
-  if (@_) {
-    ($self->{nationality}->{value},
-     $self->{nationality}->{id}) = map {encode_utf8($_)} @_;
-    return $self;
-  }
-  return $self->{nationality};
+    my $self = shift;
+
+    if (@_) {
+        ( $self->{nationality}->{value}, $self->{nationality}->{id} )
+            = map { encode_utf8($_) } @_;
+        return $self;
+    }
+    return $self->{nationality};
 }
 
 # Get/set skills.
@@ -76,21 +131,20 @@ sub nationality {
 # To get the whole hash, call with no arguments
 # To set a single skill, call with the name and value
 sub skills {
-  my $self = shift;
-  
-  if (@_) {
-    my ($name, $value) = @_;
-    $self->{skills}->{$name} = $value;
-    return $self;
-  }
-  return $self->{skills};
+    my $self = shift;
+
+    if (@_) {
+        my ( $name, $value ) = @_;
+        $self->{skills}->{$name} = $value;
+        return $self;
+    }
+    return $self->{skills};
 }
 
 # Return the full name
 sub getName {
-  my $self = shift;
-  return $self->basic('firstName') . ' ' .
-         $self->basic('lastName');
+    my $self = shift;
+    return $self->basic('firstName') . ' ' . $self->basic('lastName');
 }
 
 1;
